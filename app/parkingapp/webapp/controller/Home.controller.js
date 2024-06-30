01729079dde8d8ba7b8a8953546ba585e83d1ad7
Sreeram Sreedhar <sreeramsreedhar820@gmail.com>
2024-06-30 08:32:36 +0000

    saved on 30

--- app/parkingapp/webapp/controller/Home.controller.js
+++ app/parkingapp/webapp/controller/Home.controller.js
@@ -1,12 +1,41 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/odata/v2/ODataModel"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller, ODataModel) {
+        "use strict";
 
-    return Controller.extend("com.app.parkingapp.controller.Home", {
-        onInit: function () {
+        return Controller.extend("com.app.parkingapp.controller.Home", {
+            onInit: function () {
 
-        }
+
+            },
+            // Controller logic
+            onItemSelect: function (oEvent) {
+                var itemKey = oEvent.getParameter("item").getKey();
+                var navContainer = this.getView().byId("idNavContainer");
+
+                // Navigate to the corresponding page based on the selected key
+                switch (itemKey) {
+                    case "RouteParkingSlot":
+                        navContainer.to(this.getView().createId("root3"));
+                        break;
+                    case "Home":
+                        navContainer.to(this.getView().createId("root2"));
+                        break;
+                    case "RouteReserve":
+                        navContainer.to(this.getView().createId("root4"));
+                        break;
+                    case "parkingslotAssign":
+                        navContainer.to(this.getView().createId("idparkingassign"));
+                        break;
+                    case "root1":
+                        navContainer.to(this.getView().createId("root1"));
+                    default:
+                        break;
+                }
+            }
+
+
+        });
     });
-});
